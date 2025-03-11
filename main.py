dbf21802ff9623f97d5b92e86842c862d2cb3cac
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-11 13:42:26 +0100

    visione di funzione if __name__ == "__main__": per testing

---
+++ main.py
@@ -0,0 +1,15 @@
+# punto di ingresso da cui parte tutto il resto.
+# tutto il codice sta nella cartella SRC mentre il MAIN sta fuori.
+
+# from "nome_cartella.nome_sottoCartella.nome_file" import "nome_funzione"
+from src.utils.utils import utility_function_1, utility_function_2
+
+
+
+
+
+# NAME = variabile standard che non va definita. 
+# eseguendo il main ritorna True.
+if __name__ == "__main__":
+
+    utility_function_1()
